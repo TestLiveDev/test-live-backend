2a6f67fdd97fe39083ae53b16bf80c9eb3cb7e87
Anton <63553118+pyKorgot@users.noreply.github.com>
2025-09-09 15:19:41 +0000

    Add noqa for import
    - Models directory in file __init__.py add noqa

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 from .organization import Oraganization  # noqa
 from .release import Release  # noqa
 from .result_test_case import ResultTestCase  # noqa
-from .result_test_step import ResultTestStep
+from .result_test_step import ResultTestStep  # noqa
 from .status import Status  # noqa
 from .test_case import TestCase  # noqa
 from .test_plan import TestPlan  # noqa
