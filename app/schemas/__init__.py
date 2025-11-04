bd8c47b390f725a0e82c9d99addff1a7ea912646
Menzhulov <shutkadada@yandex.ru>
2025-11-04 22:13:53 +0300

    feat: Create Release Interface
    - add test plan api
    - add test plan in router
    - add schemas test plan

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .organization import OrganizationCreate, OrganizationUpdate, OrganizationDelete, OrganizationDBQuery  # noqa
 from .release import ReleaseCreate, ReleaseUpdate, ReleaseDelete, ReleaseDBQuery  # noqa
+from .test_plan import TestPlanCreate, TestPlanUpdate, TestPlanDelete, TestPlanDBQuery  # noqa
