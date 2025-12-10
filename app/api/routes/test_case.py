59325b548dd1a11cd817c04f2670951ee1939143
Menzhulov <shutkadada@yandex.ru>
2025-12-10 20:42:01 +0300

    Refactor TestPlan API To Class; Add TestStep API

--- app/api/routes/test_case.py
+++ app/api/routes/test_case.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.deps import get_db
-from app.schemas import TestCaseCreate
+from app.schemas import TestCaseCreate, TestCaseDelete
 from app.crud import crud_test_case
+from app.models import TestCase
 
 
 router = APIRouter(prefix='/test_case', tags=['Test Case'])
@@ -12,5 +13,12 @@ router = APIRouter(prefix='/test_case', tags=['Test Case'])
 @router.post('')
 async def add_test_case(item: TestCaseCreate, db: AsyncSession = Depends(get_db)):
     await crud_test_case.create(db, dict(item))
+    await db.commit()
+    return True
+
+
+@router.delete('')
+async def del_organization(item: TestCaseDelete, db: AsyncSession = Depends(get_db)):
+    await crud_test_case.delete(db, TestCase.id_test_case == item.id_test_case)
     await db.commit()
     return True
--- app/schemas/test_case.py
+++ app/schemas/test_case.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class TestCaseCreate(BaseModel):
     id_test_plan: int
     name_test_case: str
+
+
+class TestCaseDelete(BaseModel):
+    id_test_case: int
